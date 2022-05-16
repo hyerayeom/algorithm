d1e0d93443fc1fbf1d3553d0c3e9fe81004fe06b
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2022-05-16 19:50:55 +0900

    [Bronze II] Title: 단어의 개수, Time: 524 ms, Memory: 38240 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+
+		String S = scan.nextLine();
+		S = S.trim();
+		String answer[] = S.split(" ");
+		if(answer.length == 1 && answer[0].equals("")) {
+			System.out.println(0);
+		}else {
+			System.out.println(answer.length);
+		}
+	}
+}
