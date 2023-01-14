e4316b8adcf82a592a3c9a35fa0894717ee7edf8
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2023-01-14 22:08:53 +0900

    [Bronze V] Title: A+B - 4, Time: 252 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int A, B, ans;
+		
+		 while(scan.hasNextInt()) {
+			 A=scan.nextInt();
+			 B=scan.nextInt();
+			 ans=A+B;
+			 System.out.println(ans);
+		 }
+	}
+}
