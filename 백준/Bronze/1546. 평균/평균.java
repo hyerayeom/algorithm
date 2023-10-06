8a11f62b2b79b05aaa7d8d9fac4c224004fbab3c
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2023-10-07 01:48:12 +0900

    [Bronze I] Title: 평균, Time: 356 ms, Memory: 25236 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);
 
-        int N = scan.nextInt();
-        double[] score = new double[N];
-
-        for (int i = 0; i < N; i++) {
-            score[i] = scan.nextInt();
+        int N=scan.nextInt();
+        double[] score=new double[N];
+        for(int i=0;i<N;i++){
+            score[i]=scan.nextDouble();
         }
         Arrays.sort(score);
-        double max = score[N-1];
+        double max=score[N-1];
+
         double sum=0;
-        for (int j = 0; j < N; j++) {
-            score[j] = (score[j]/max) * 100;
-            sum+=score[j];
+        double answer=0;
+        for(int i=0;i<N;i++){
+            score[i]=(score[i]/max)*100;
+            sum+=score[i];
+            if(i==N-1){
+                answer=sum/N;
+            }
         }
-        double average=sum/(double)N;
-        System.out.println(average);
+        System.out.println(answer);
     }
 }
