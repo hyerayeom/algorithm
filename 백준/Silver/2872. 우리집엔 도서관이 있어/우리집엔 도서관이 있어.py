de0778d513052fbbf631c1e9102154165a313838
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2025-01-11 23:35:49 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 164 ms, Memory: 45192 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
@@ -1,15 +1,16 @@
-from collections import deque
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-nums = deque()
+books = []
+count = 0
 
 for _ in range(n) :
-  nums.append(int(input()))
-count = 0
-while nums :
-  if nums.pop() == n :
+  books.append(int(input()))
+
+while books :
+  if books.pop() == n :
     n -= 1
   else :
     count += 1
