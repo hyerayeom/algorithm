539e9f90824ec5680ee223c50fc4ea6b544d3d1f
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2025-01-18 16:05:39 +0900

    [Silver II] Title: 좌표 압축, Time: 1824 ms, Memory: 149248 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -3,15 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
-sorted_nums = sorted(nums)
-count = {sorted_nums[0]:0}
+sorted_nums = sorted(set(nums))
+count = {}
 
-for i in range(1,n) :
-  if sorted_nums[i] == sorted_nums[i-1] :
-    continue
-  count[sorted_nums[i]] = count[sorted_nums[i-1]] + 1
+for i in range(len(sorted_nums)) :
+  count[sorted_nums[i]] = i
 
-answer =[]
 for i in nums :
-  answer.append(count[i])
-print(*answer)
+  print(count[i], end=" ")
