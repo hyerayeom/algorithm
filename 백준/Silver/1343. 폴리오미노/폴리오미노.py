1f2dfe37f672e7a568d4ea18bb2b1d83e12cbe26
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2025-01-20 23:49:54 +0900

    [Silver V] Title: 폴리오미노, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -2,28 +2,12 @@ import sys
 input = sys.stdin.readline
 
 line = input().strip()
-answer = ""
-strA = "AAAA"
-strB = "B"
 
-count = 0
+line = line.replace("XXXX", "AAAA")
+line = line.replace("XX", "BB")
+
 for l in line :
-  if l == "X" : 
-    count += 1
-  else :
-    if count % 2 : 
-      answer = "-1"
-      break
-    if count != 0 :
-      answer += strA*(count//4)
-      answer += strB*(count%4)
-      count = 0
-    answer += "."
-    
-if count != 0 :
-  if count % 2 : 
-    answer = "-1"
-  else :
-    answer += strA*(count//4)
-    answer += strB*(count%4)    
-print(answer)
+  if l == "X" :
+    line = "-1"
+
+print(line)
