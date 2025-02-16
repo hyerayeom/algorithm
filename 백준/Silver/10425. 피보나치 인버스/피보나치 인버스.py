25ecbf28965ee59dcbbb13a4f75177a59842cd89
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2025-02-16 16:52:41 +0900

    [Silver II] Title: 피보나치 인버스, Time: 620 ms, Memory: 549200 KB -BaekjoonHub

--- 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
+++ 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
@@ -3,12 +3,21 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = [int(input()) for _ in range(n)]
-fivo = {}
+max_n = 10**5
 
-memo = [0,1] + [0]*(10**5-1)
-for i in range(2,10**5+1):
+memo = [0,1] + [0]*(max_n-1)
+for i in range(2, max_n+1):
   memo[i] = memo[i-1] + memo[i-2]
-  fivo[memo[i]] = i
 
-for i in nums:
-  print(fivo[i])
+for num in nums:
+  start = -1
+  end = max_n
+  while start < end-1:
+    mid = (start+end) // 2
+    if memo[mid] < num:
+      start = mid
+    else:
+      end = mid
+  if end == 1:
+    end = 2
+  print(end)
