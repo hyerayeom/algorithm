e5d1b3282872e59d20ed4142e3d8210bc24db559
Yeom hyera <75641790+Yeomhyera@users.noreply.github.com>
2025-02-22 22:44:42 +0900

    [Gold IV] Title: DSLR, Time: 7800 ms, Memory: 224620 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -2,33 +2,32 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(n, m):
-  operation = ["D", "S", "L", "R"]
-  visited = set()
-  visited.add(n)
+def bfs(n,m):
+  operation = ["D","S","L","R"]
+  
+  visited = [0] * (10**5)
+  visited[n] = 1
 
   q = deque()
-  q.append((n, ""))
+  q.append((n,""))
 
   while q:
     now, oper = q.popleft()
     if now == m:
-        return oper
+      return oper
     
-    d = (now * 2) % 10000
-    s = now - 1 if now != 0 else 9999
-    l = (now % 1000) * 10 + now // 1000
-    r = (now % 10) * 1000 + now // 10
-    res = [d, s, l, r]
+    d = (now*2)%(10**4)
+    s = now-1 if now != 0 else 9999
+    l = now%1000*10 + now//1000
+    r = now%10*1000 + now//10
+    res = [d,s,l,r]
 
     for i in range(4):
-      if res[i] not in visited:
-        if res[i] == m:
-          return f"{oper}{operation[i]}"
-        q.append((res[i], f"{oper}{operation[i]}"))
-        visited.add(res[i])
+      if not visited[res[i]]:
+        q.append((res[i], oper+operation[i]))
+        visited[res[i]] = 1
 
 test_case = int(input())
 for _ in range(test_case):
-    n, m = map(int, input().split())
-    print(bfs(n, m))
+  n,m = map(int, input().split())
+  print(bfs(n,m))
